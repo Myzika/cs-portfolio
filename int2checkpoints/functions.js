646c3ae52da55ffdba2c6b598d8c6732f2e69791
Jordan Wong <ajordaninlimbo@gmail.com>
2018-01-10 18:44:30 +0000

    Added the files for functions, checkpoint 4

---
+++ int2checkpoints/functions.js
@@ -0,0 +1,14 @@
+/* global draw ellipse rect p processing width height size */
+var sketch = function(processing) {
+    with(processing) {
+        size(400, 400);
+        background(255);
+        // °º¤ø,¸¸,ø¤º°`°º¤ø Create Project Below This Line ø¤º°`°º¤ø,¸,ø¤°//
+        draw = function() {
+            ellipse(200, 200, 15, 15);
+        };
+
+        // °º¤ø,¸¸,ø¤º°`°º¤ø Create Project Above This Line ø¤º°`°º¤ø,¸,ø¤°//   
+    }
+};
+var p = new Processing(document.getElementById("output-canvas"), sketch);
