6e27a8245ca6790fec4aadc2467875ca2c2e56c1
Jordan Wong <ajordaninlimbo@gmail.com>
2018-01-11 18:12:49 +0000

    Set up the files for final draft

--- int2checkpoints/functions.js
+++ int2checkpoints/functions.js
@@ -3,9 +3,10 @@ var sketch = function(processing) {
     with(processing) {
         size(400, 400);
         background(255);
-        // °º¤ø,¸¸,ø¤º°`°º¤ø Create Project Below This Line ø¤º°`°º¤ø,¸,ø¤°//
         
+        // °º¤ø,¸¸,ø¤º°`°º¤ø Create Project Below This Line ø¤º°`°º¤ø,¸,ø¤°//
         draw = function() {
+            
             /**
              * Contains all of the needed functions in order
              * to produce the base image.
@@ -13,6 +14,7 @@ var sketch = function(processing) {
              * @param {int} y Y of the location of the mouse click
              */ 
             drawMe = function(x, y) {
+                
                 /**
                  * Randomized variables that determine the
                  * hue of each sections.
@@ -27,36 +29,46 @@ var sketch = function(processing) {
                 /*Resetting the borders*/
                 strokeWeight(1);
                 stroke(0, 0, 0);
+                
                 //Red
                 fill(color, 0, 0);
                 ellipse(x, y, size, size);
+                
                 //Orange
                 fill(highcolor, lowcolor, 0);
                 ellipse(x, y, size - 10, size - 10);
+                
                 //Yellow
                 fill(highcolor, highcolor, 0);
                 ellipse(x, y, size - 20, size - 20);
+                
                 //Green
                 fill(0, color, 0);
                 ellipse(x, y, size-30, size-30);
+                
                 //Blue
                 fill(0, 0, color);
                 ellipse(x, y, size-40, size-40);
+                
                 //indigo
                 fill(lowcolor, 0, highcolor);
                 ellipse(x, y, size-50, size-50);
+                
                 //Violet
                 fill(color, 0, highcolor);
                 ellipse(x, y, size-60, size-60);
+                
                 //Stick
                 strokeWeight(3);
                 stroke(random(255), random(255), random(255));
                 line(x, y + size/2, x, y + 100);
             };
+            
             /**
-             * This function calls drawMe when the mouse is clicked
+             * @func mouseClicked Calls drawMe() when the mouse is clicked.
              */
             mouseClicked = function() {
+                
                 /**
                  * Calls @func drawMe
                  * @arg {int} mouseX the x value of the location of the mouse click
@@ -65,6 +77,7 @@ var sketch = function(processing) {
                 drawMe(mouseX, mouseY);
             };
         };
+        
         // °º¤ø,¸¸,ø¤º°`°º¤ø Create Project Above This Line ø¤º°`°º¤ø,¸,ø¤°//   
     }
 };
