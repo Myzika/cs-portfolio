f163d90f9a3f09aceea08d7dc3cbe120606e90dc
Jordan Wong <ajordaninlimbo@gmail.com>
2018-01-12 18:59:01 +0000

    Finished my first tree!

--- int2checkpoints/testing/drawone.js
+++ int2checkpoints/testing/drawone.js
@@ -4,15 +4,60 @@ var sketch = function(processing) {
         size(screen.width, screen.height);
         // °º¤ø,¸¸,ø¤º°`°º¤ø Create Project Below This Line ø¤º°`°º¤ø,¸,ø¤°//
         background(255);
+
+        /** Combines the parts of the tree to draw full trees */
         trees = function() {
+            leaves(); //draws the leaves
+            trunks(); //draws the tree trunks
+            branches(); //draws the branches
+        };
+
+        /** Draws all of the tree trunks */
+        trunks = function() {
             var d = 200;
             var i = 600;
+            var a;
             stroke(84, 56, 34);
             strokeWeight(20);
             noFill();
-            curve(5, screen.height -300, 73, 200 + d, 73, 80 + i, 15, 80 + i);
-            //curve(5, screen.height + d, 73, 200 +d, 73, 61 + d, 15, 65 + d);
+            curve(5, screen.height - 300, 73, 200 + d, 73, 80 + i, 15, 80 + i);
+            fill(84, 56, 34);
+            // noStroke();
+            triangle(80, screen.height -200, 30, 800, 130, 750);
         };
+
+        /** Draws thicker main branches and thinner branches */
+        branches = function() {
+            //Main Branches
+            strokeWeight(6);
+            stroke(84, 56, 34);
+            triangle(67, 450, 67, 452, 30, 445);
+            triangle(70, 400, 70, 402, 130, 390);
+            triangle(30, 330, 30, 330, 70, 400);
+
+            //Smaller Branches
+            strokeWeight(4);
+            triangle(128, 390, 140, 370, 140, 370);
+            triangle(128, 390, 150, 410, 150, 410);
+            line(60, 320, 54, 370);
+        };
+
+        /** Creates the leaves for the trees */
+        leaves = function() {
+            noStroke();
+            fill(10, 71, 23);
+            ellipse(80, 400, 220, 130);
+            ellipse(120, 350, 100, 80)
+            fill(22, 73, 25);
+            ellipse(30, 445, 70, 40);
+            fill(11, 66, 17);
+            ellipse(45, 330, 90, 80);
+            fill(0, 70, 9);
+            ellipse(140, 383, 120,80);
+            
+        };
+
+        /** Creates a gradient for the sky*/
         sky = function() {
             //Color Variables for the sky
             var r = 50;
@@ -20,7 +65,6 @@ var sketch = function(processing) {
             var b = 110;
 
             //The 'start' of the sky
-
             noStroke();
             fill(r, g, b);
             rect(0, 0, screen.width, 25);
@@ -35,7 +79,9 @@ var sketch = function(processing) {
                 g -= 2;
                 b -= 2;
             }
-        }
+        };
+
+        /** Makes the ground */
         ground = function() {
             /**
              * Draws small circles thawt repeat for the ground.
@@ -45,17 +91,16 @@ var sketch = function(processing) {
                 fill(38, 34, 30);
                 ellipse(i, screen.height - 80, 150, 200);
             }
-        }
-        
-        /**
-         * Main function
-         */ 
+        };
+
+        /** Main function */
         draw = function() {
-            //Frame One
+
+            /** Frame One */
             frameOne = function() {
-                sky();      //draws the sky
-                trees();    //draws the trees
-                ground();   //draws the ground
+                sky(); //draws the sky
+                trees(); //draws the trees
+                ground(); //draws the ground
             };
             frameOne();
         };
