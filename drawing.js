753f0e047cd18e37d52c469765aab8e22b2e9576
Jordan Wong <ajordaninlimbo@gmail.com>
2018-01-19 18:27:15 +0000

    Added to aboutme, more optimizations.

--- drawing.js
+++ drawing.js
@@ -15,68 +15,52 @@ var sketch = function(processing) {
             noFill();
 
             /**Creates the arrow at the beginning*/
-            arrow = function() {
-                line(300, y / 2, 300, y / 2 + 100);
-                line(300, y / 2, 270, y / 2 + 30);
-                line(300, y / 2, 330, y / 2 + 30);
-            }
+            line(300, y / 2, 300, y / 2 + 100);
+            line(300, y / 2, 270, y / 2 + 30);
+            line(300, y / 2, 330, y / 2 + 30);
+
 
             /**Creates the letter t*/
-            t = function() {
-                line(375, y / 2 + 10, 375, y / 2 + 90);
-                line(355, y / 2 + 30, 395, y / 2 + 30);
-            };
+            line(375, y / 2 + 10, 375, y / 2 + 90);
+            line(355, y / 2 + 30, 395, y / 2 + 30);
+
 
             /**Creates the letter o*/
-            o = function() {
-                ellipse(420, y / 2 + 65, 55, 55);
-            };
+            ellipse(420, y / 2 + 65, 55, 55);
+
 
             /**Creates the letter b*/
-            b = function() {
-                ellipse(508, y / 2 + 65, 55, 55);
-                line(480, y / 2 + 10, 480, y / 2 + 90);
-                line(490, y / 2 + 10, 490, y / 2 + 90);
-            };
+            ellipse(508, y / 2 + 65, 55, 55);
+            line(480, y / 2 + 10, 480, y / 2 + 90);
+            line(490, y / 2 + 10, 490, y / 2 + 90);
+
 
             /**Creates the letter e*/
-            e = function() {
-                line(550, y / 2 + 40, 550, y / 2 + 90);
-                line(560, y / 2 + 40, 560, y / 2 + 90);
-                line(550, y / 2 + 38, 580, y / 2 + 38);
-                line(550, y / 2 + 92, 580, y / 2 + 92);
-                line(550, y / 2 + 65, 580, y / 2 + 65);
-            };
+            line(550, y / 2 + 40, 550, y / 2 + 90);
+            line(560, y / 2 + 40, 560, y / 2 + 90);
+            line(550, y / 2 + 38, 580, y / 2 + 38);
+            line(550, y / 2 + 92, 580, y / 2 + 92);
+            line(550, y / 2 + 65, 580, y / 2 + 65);
+
 
             /**Creates the letter g*/
-            g = function() {
-                ellipse(620, y / 2 + 65, 55, 55);
-                line(647, y / 2 + 60, 647, y / 2 + 120);
-                arc(622, 500, 50, 50, 0, 4 * PI / 5);
-            };
+            ellipse(620, y / 2 + 65, 55, 55);
+            line(647, y / 2 + 60, 647, y / 2 + 120);
+            arc(622, 500, 50, 50, 0, 4 * PI / 5);
+
 
             /**Creates the letter i*/
-            i = function() {
-                line(680, y / 2 + 40, 680, y / 2 + 90);
-                line(658, y / 2 + 38, 678, y / 2 + 38);
-                line(670, y / 2 + 40, 670, y / 2 + 90);
-                ellipse(672.5, y / 2 + 20, 15, 15);
-            };
+            line(680, y / 2 + 40, 680, y / 2 + 90);
+            line(658, y / 2 + 38, 678, y / 2 + 38);
+            line(670, y / 2 + 40, 670, y / 2 + 90);
+            ellipse(672.5, y / 2 + 20, 15, 15);
+
 
             /**Creates the letter n*/
-            n = function() {
-                line(700, y / 2 + 40, 700, y / 2 + 90);
-                line(700, y / 2 + 40, 730, y / 2 + 90);
-                line(730, y / 2 + 40, 730, y / 2 + 90);
-            }
-            arrow();
-            t();
-            o();
-            b()
-            e()
-            g();
-            i();
-            n();
+            line(700, y / 2 + 40, 700, y / 2 + 90);
+            line(700, y / 2 + 40, 730, y / 2 + 90);
+            line(730, y / 2 + 40, 730, y / 2 + 90);
+
         };
 
         /**
@@ -159,15 +143,15 @@ var sketch = function(processing) {
                         curve(5 + move, screen.height - 300, 73 + move, 400, 73 + move, 680, 15 + move, 680);
                         fill(84, 56, 34);
                         triangle(80 + move, screen.height - 200, 30 + move, 800, 130 + move, 750);
-                       
+
                     };
-                     tree(0);
-                        tree(100);
-                        tree(screen.width - 100);
-                        tree(screen.width - 220);
-                        tree(screen.width - 380);
-                        tree(screen.width - 300);
-                        tree(screen.width - 500);
+                    tree(0);
+                    tree(100);
+                    tree(screen.width - 100);
+                    tree(screen.width - 220);
+                    tree(screen.width - 380);
+                    tree(screen.width - 300);
+                    tree(screen.width - 500);
                 };
 
                 /** Draws thicker main branches and thinner branches */
