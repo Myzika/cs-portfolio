263235bfcfc5d440d085f3eca3ca4f76ebfcff9c
Jordan Wong <ajordaninlimbo@gmail.com>
2018-03-19 16:35:25 +0000

    Added button + text input and hooked into JS

---
+++ int3checkpoints/colorchanger.js
@@ -0,0 +1,5 @@
+//Variable that stores the DOM ID input (the HTML text input)
+var textInput1 = document.getElementById("input");
+
+//Variable that stores the DOM ID button (the HTML button)
+var clickMe = document.getElementById("button");
