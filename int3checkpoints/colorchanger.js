156de3687655478808b0b4a53152cd90297fd8b5
Jordan Wong <ajordaninlimbo@gmail.com>
2018-03-19 16:42:44 +0000

    Met the basic requirements for int3checkpoint 1, colorchanger

--- int3checkpoints/colorchanger.js
+++ int3checkpoints/colorchanger.js
@@ -1,5 +1,10 @@
 //Variable that stores the DOM ID input (the HTML text input)
-var textInput1 = document.getElementById("input");
+var input = document.getElementById("input");
 
 //Variable that stores the DOM ID button (the HTML button)
 var clickMe = document.getElementById("button");
+
+clickMe.addEventListener("click", colorChanger);
+function colorChanger(){
+    document.body.style.backgroundColor = input.value;
+}
