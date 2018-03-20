05dcad75814fd940f0e2c27c9fe05446d866731d
Jordan Wong <ajordaninlimbo@gmail.com>
2018-03-20 15:52:39 +0000

    started working on info page

--- int3checkpoints/colorchanger.js
+++ int3checkpoints/colorchanger.js
@@ -4,7 +4,10 @@ var input = document.getElementById("input");
 //Variable that stores the DOM ID button (the HTML button)
 var clickMe = document.getElementById("button");
 
+//Adds event listener on-click button call colorChanger
 clickMe.addEventListener("click", colorChanger);
+
+//Sets the background color equal to input.value
 function colorChanger(){
     document.body.style.backgroundColor = input.value;
 }
---
+++ int3checkpoints/info.js
@@ -0,0 +1,6 @@
+//Sets alcohol image to alcohol
+var alcohol = document.getElementById("alcohol");
+
+//Sets coding ingame to code
+var code = document.getElementById("code");
+
